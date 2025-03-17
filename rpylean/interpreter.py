8adfc56f32af5a233d1c81af57afa9331ee984bd
Aaron Hill <aa1ronham@gmail.com>
2025-03-16 23:43:02 -0400

    Implement more types

--- rpylean/interpreter.py
+++ rpylean/interpreter.py
@@ -2,14 +2,30 @@ from __future__ import print_function
 
 from rpylean.parser import parse
 
+class Level:
+    pass
 
 class LevelZero:
     pass
 
+class LevelParam:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "LevelParam(%s)" % (self.name,)
 
 class Expr:
     pass
 
+class RecRule:
+    def __init__(self, name, nfields, rhs):
+        self.name = name
+        self.nfields = nfields
+        self.rhs = rhs
+
+    def __repr__(self):
+        return "RecRule(%s, %s, %s)" % (self.name, self.nfields, self.rhs)
 
 class ExprSort(Expr):
     def __init__(self, level):
@@ -26,6 +42,32 @@ class ExprConst(Expr):
     def __repr__(self):
         return "ExprConst(%s)" % (self.const,)
 
+class ExprForallE(Expr):
+    def __init__(self, binder_info, binder_name, binder_type, body):
+        self.binder_info = binder_info
+        self.binder_name = binder_name
+        self.binder_type = binder_type
+        self.body = body
+
+class ExprLambda(Expr):
+    def __init__(self, binder_info, binder_name, binder_type, body):
+        self.binder_info = binder_info
+        self.binder_name = binder_name
+        self.binder_type = binder_type
+        self.body = body
+
+class ExprBVar(Expr):
+    def __init__(self, id):
+        self.id = id
+
+class ExprApp(Expr):
+    def __init__(self, function, arg):
+        self.function = function
+        self.arg = arg
+
+    def __repr__(self):
+        return "ExprApp(%s, %s)" % (self.function, self.arg)
+        # return "ExprApp(%s)" % (self.function,)
 
 class Environment:
     def __init__(self):
@@ -33,6 +75,7 @@ class Environment:
         self.exprs = {}
         self.names = {"0": []}
         self.constants = {}
+        self.rec_rules = {}
 
     def __repr__(self):
         return "Environment()"
@@ -48,6 +91,14 @@ class Environment:
     def add_constant(self, name, type):
         assert name not in self.constants
         self.constants[name] = type
+
+    def add_level(self, uidx, level):
+        assert uidx not in self.levels
+        self.levels[uidx] = level
+
+    def add_rec_rule(self, ridx, name, nfields, rhs):
+        assert ridx not in self.rec_rules
+        self.rec_rules[ridx] = RecRule(name, nfields, rhs)
 
 
 def interpret(source):
@@ -77,6 +128,29 @@ def interpret(source):
                 assert len(item.children) == 3, "uidxs present"
                 value = env.names[nidx]
                 env.add_expr(eidx, ExprConst(value))
+            elif etype.additional_info == "#EP":
+                eidx = item.children[0].children[0].additional_info
+                info = item.children[2].children[0].additional_info
+                binderName = env.names[item.children[3].children[0].additional_info]
+                binderType = env.exprs[item.children[4].children[0].additional_info]
+                body = env.exprs[item.children[5].children[0].additional_info]
+                env.add_expr(eidx, ExprForallE(info, binderName, binderType, body))
+            elif etype.additional_info == "#EV":
+                eidx = item.children[0].children[0].additional_info
+                id = item.children[2].additional_info
+                env.add_expr(eidx, ExprBVar(id))
+            elif etype.additional_info == "#EA":
+                eidx = item.children[0].children[0].additional_info
+                function = env.exprs[item.children[2].children[0].additional_info]
+                arg = env.exprs[item.children[3].children[0].additional_info]
+                env.add_expr(eidx, ExprApp(function, arg))
+            elif etype.additional_info == "#EL":
+                eidx = item.children[0].children[0].additional_info
+                info = item.children[2].children[0].additional_info
+                binderName = env.names[item.children[3].children[0].additional_info]
+                binderType = env.exprs[item.children[4].children[0].additional_info]
+                body = env.exprs[item.children[5].children[0].additional_info]
+                env.add_expr(eidx, ExprLambda(info, binderName, binderType, body))
             else:
                 assert False, etype
         elif item.symbol == "declaration":
@@ -86,7 +160,23 @@ def interpret(source):
             type = env.exprs[eidx]
             key = ".".join(name)
             env.add_constant(key, type)
+        elif item.symbol == "universe":
+            target_uidx = item.children[0].children[0].additional_info
+            utype = item.children[1]
+            if utype.additional_info == "#UP":
+                uname = item.children[2].children[0].additional_info
+                env.add_level(target_uidx, LevelParam(uname))
+            else:
+                raise RuntimeError("unknown universe type %s" % (utype.additional_info,))
+        elif item.symbol == "recrule":
+            target_ridx = item.children[0].children[0].additional_info
+            name = env.names[item.children[2].children[0].additional_info]
+            nfields = item.children[3].additional_info
+            rhs = env.exprs[item.children[4].children[0].additional_info]
+            env.add_rec_rule(target_ridx, name, nfields, rhs)
+            print("Rule", target_ridx, name, nfields, rhs)
         else:
+            print("Symbol", item.symbol)
             assert False, each
 
     print("NAMES:")
