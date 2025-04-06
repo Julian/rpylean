53bd2ace9a1ac2fc512ba324624611a96e43be8f
Julian Berman <Julian@GrayVines.com>
2025-04-06 10:43:07 -0400

    Jiggle around arg parsing.

--- rpylean/cli.py
+++ rpylean/cli.py
@@ -1,4 +1,8 @@
+"""
+CLI for rpylean.
+"""
 from __future__ import print_function
+
 import errno
 
 from rpython.rlib.streamio import open_file_as_stream
@@ -7,26 +11,55 @@ from rpython.rlib.rfile import RFile, c_stderr
 from rpylean.interpreter import interpret
 
 
+USAGE = """
+%s EXPORT_FILE
+""".rstrip("\n")
+
+
+class UsageError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+    @staticmethod
+    def with_message(message, executable):
+        if executable.endswith("__main__.py"):
+            executable = "pypy -m rpylean"
+        usage = USAGE % (executable,)
+        return UsageError("%s, run:\n%s" % (message, usage))
+
+
 def main(argv):
-    if len(argv) != 2:
-        print("Run %s <export-file>." % (argv[0],))
+    try:
+        lines = parse_args(argv)
+    except UsageError as error:
+        stderr = RFile(c_stderr(), close2=(None, None))
+        stderr.write(error.__str__())
+        stderr.write("\n")
         return 1
+
+    interpret(lines)
+    return 0
+
+
+def parse_args(argv):
+    if len(argv) != 2:
+        raise UsageError.with_message("Wrong number of arguments", argv[0])
 
     path = argv[1]
     try:
-        f = open_file_as_stream(path)
+        file = open_file_as_stream(path)
     except OSError as err:
         if err.errno != errno.ENOENT:
             raise
-        stderr = RFile(c_stderr(), close2=(None, None))
-        stderr.write("%s does not exist.\n" % (path,))
-        return 1
+        raise UsageError("`%s` does not exist." % (path,))
 
-    try:
-        interpret(f.readall().splitlines())
-    finally:
-        f.close()
-    return 0
+    lines = file.readall().splitlines()
+    file.close()
+    return lines
+
 
 if __name__ == '__main__':
     import sys
