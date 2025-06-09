27cb875f60803d789dc80f8d902a01e90e2de290
Julian Berman <Julian@GrayVines.com>
2025-06-09 18:26:00 -0400

    Slighty spiffier REPL.

--- rpylean/repl.py
+++ rpylean/repl.py
@@ -2,9 +2,93 @@
 Interactive REPL for rpylean.
 """
 from rpython.rlib.rfile import create_stdio
-import re
 
 from rpylean.objects import W_TypeError, Name
+
+
+COMMANDS, HELP = {}, {}
+
+def command(names, help=None):
+    assert help is not None, names
+
+    def _command(func):
+        for name in names:
+            COMMANDS[name] = func
+        HELP[names[0]] = help
+        return func
+    return _command
+
+
+@command(["dump", "d"], help="Dump the current environment.")
+def dump(env, _, __, stdout, ___):
+    env.dump_pretty(stdout)
+
+
+@command(
+    ["check", "c"],
+    help=(
+        "Type check a declaration by name, "
+        "or all declarations if no name is given."
+    ),
+)
+def check(env, args, _, stdout, __):
+    if not args:  # ok, all of them!
+        env.type_check()
+        stdout.write(
+            "Checked %d declarations.\n" % len(env.declarations),
+        )
+        return
+
+    name = Name.from_str(args[0])
+    declaration = env.declarations[name]
+    try:
+        declaration.type_check(env.inference_context())
+    except W_TypeError as error:
+        stdout.write("Type error: %s\n" % error)
+    else:
+        stdout.write("%s correctly type checks.\n" % name.pretty())
+
+
+@command(["print", "p"], help="Pretty print a declaration by name.")
+def print_decl(env, args, _, stdout, __):
+    name, = args
+    stdout.write(env[name].pretty())
+    stdout.write("\n")
+
+
+@command(
+    ["names", "n"],
+    help=(
+        "Show all names in the environment, "
+        "or those matching a prefix or length."
+    ),
+)
+def names(env, args, stdin, stdout, stderr):
+    names = env.declarations.keys()
+    if len(args) == 1:  # ok, all of them!
+        arg = args[0].strip()
+        if arg.isdigit():  # all names with at most `n` components
+            n = int(arg)
+            names = [
+                name for name in names if len(name.components) <= n
+            ]
+        else:              # all names starting with the given prefix
+            names = [
+                name for name in names if name.pretty().startswith(arg)
+            ]
+
+    for name in names:
+        stdout.write(name.pretty())
+        stdout.write("\n")
+
+
+@command(["help", "commands", "?"], help="Show the available REPL commands.")
+def help(_, __, ___, stdout, ____):
+    for command, doc in HELP.items():
+        stdout.write(command)
+        stdout.write(": ")
+        stdout.write(doc)
+        stdout.write("\n")
 
 
 def interact(env):
@@ -23,43 +107,8 @@ def interact(env):
         split = input.split(" ", 1)
         command = split[0]
 
-        if command in ["d", "dump"]:
-            env.dump_pretty(stdout)
-        elif command in ["c", "check"]:
-            if len(split) == 1:  # ok, all of them!
-                env.type_check()
-                stdout.write(
-                    "Checked %d declarations.\n" % len(env.declarations),
-                )
-                continue
-
-            name = Name.from_str(split[1])
-            try:
-                env.declarations[name].type_check(env.inference_context())
-            except W_TypeError as error:
-                stdout.write("Type error: %s\n" % error)
-            else:
-                stdout.write("%s correctly type checks.\n" % name.pretty())
-        elif command in ["p", "print"]:
-            name = split[1]
-            stdout.write(env[name].pretty())
-            stdout.write("\n")
-        elif command in ["n", "names"]:
-            names = env.declarations.keys()
-            if len(split) == 2:  # ok, all of them!
-                arg = split[1].strip()
-                if arg.isdigit():  # all names with at most `n` components
-                    n = int(arg)
-                    names = [
-                        name for name in names if len(name.components) <= n
-                    ]
-                else:              # all names starting with the given prefix
-                    names = [
-                        name for name in names if name.pretty().startswith(arg)
-                    ]
-
-            for name in names:
-                stdout.write(name.pretty())
-                stdout.write("\n")
-        else:
-            stdout.write("Unknown command: %s\n" % command)
+        fn = COMMANDS.get(command, None)
+        if fn is None:
+            stderr.write("Unknown command: %s\n" % command)
+            continue
+        fn(env, split[1:], stdin, stdout, stderr)
