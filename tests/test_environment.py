325b3194890abc45ab5fd1df1c50450077e35aeb
Julian Berman <Julian@GrayVines.com>
2025-06-14 15:46:17 -0400

    Minor test simplification.

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,30 +1,26 @@
-from textwrap import dedent
-
 import pytest
 
 from rpylean.environment import Environment
-from rpylean.objects import W_LEVEL_ZERO, W_TypeError, W_Definition
+from rpylean.objects import PROP, TYPE, W_LEVEL_ZERO, W_TypeError, W_Definition
 
 
 def test_valid_def_type_checks():
-    W_Type = W_LEVEL_ZERO.succ().sort()
-    W_Type1 = W_LEVEL_ZERO.succ().succ().sort()
-
-    valid = W_Definition(type=W_Type1, value=W_Type, hint="R")
+    """
+    Prop : Type
+    """
+    valid = W_Definition(type=TYPE, value=PROP, hint="R")
     valid.type_check(Environment.EMPTY)
 
 
 def test_invalid_def_does_not_type_check():
     """
-    def test : Type := Type is not type correct.
+    Type is not a Prop.
     """
 
-    W_Prop = W_LEVEL_ZERO.sort()
-    W_Type = W_LEVEL_ZERO.succ().sort()
-    W_Type1 = W_LEVEL_ZERO.succ().succ().sort()
-
-    invalid = W_Definition(type=W_Prop, value=W_Type, hint="R")
+    invalid = W_Definition(type=PROP, value=TYPE, hint="R")
 
     with pytest.raises(W_TypeError) as e:
         invalid.type_check(Environment.EMPTY)
-    assert e.value.w_expected_type == W_Type1
+
+    TYPE_1 = W_LEVEL_ZERO.succ().succ().sort()
+    assert e.value.w_expected_type == TYPE_1
